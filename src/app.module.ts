e074ff1d82d59b254feb0c1e4cce5e3edf0ab2e1
Steven Yung <stevenyung@icloud.com>
2019-09-28 01:25:00 +0200

    add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigService } from './config/config.service';
+import * as path from 'path';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: ConfigService,
+      useValue: new ConfigService(path.resolve(__dirname, '..', '.env')),
+    },
+  ],
 })
 export class AppModule {}
