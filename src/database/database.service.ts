a65b11f411a54bf91b867f2626a66ee017dcbd4d
Steven Yung <stevenyung@icloud.com>
2019-10-07 22:26:12 +0200

    resolve db file

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import * as low from 'lowdb';
 import * as FileSync from 'lowdb/adapters/FileSync';
+import * as path from "path";
 
 @Injectable()
 export class DatabaseService {
@@ -8,7 +9,7 @@ export class DatabaseService {
   private readonly db;
 
   constructor(defaults: object = {}) {
-    this.adapter = new FileSync('db.json');
+    this.adapter = new FileSync(path.resolve('db.json'));
     this.db = low(this.adapter);
     this.db.defaults(defaults).write();
   }
